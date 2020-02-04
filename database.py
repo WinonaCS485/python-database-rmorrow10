429fc93fd9a274be68f0cb0c6be82d6733aae212
rmorrow10 <rmorrow210@outlook.com>
2020-02-03 21:03:05 -0600

    Modified the database.py file to connect to the SQL university database.

--- database.py
+++ database.py
@@ -2,17 +2,18 @@ import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='mrbartucz.com',
-                             user='CS485',
-                             password='WinonaState',
-                             db='CS485',
+                             user='rk6239hx',
+                             password='us10interstate39',
+                             db='rk6239hx_university',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 try:
     with connection.cursor() as cursor:
-        # Select all Students
-        sql = "SELECT * from Students"
-        
+        # Search for a student
+        searchName = input("Search for the name of the student: ")
+        sql = "SELECT * FROM Students WHERE FirstName = " + "'" + searchName + "'"
+                
         # execute the SQL command
         cursor.execute(sql)
         
